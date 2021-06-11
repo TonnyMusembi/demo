2ae93b20d6a4fa5665b2eb6c4aabd9feb7661ff8
Tonny <musembitonnyj@gmail.com>
2021-06-11 12:24:58 +0300

    commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+<nav>
+  <nav class="navbar navbar-inverse">
+    <ul class="nav menu">
+      <li>
+        <router-link to="/about" @click.native="closeNavBarFromChild">About</router-link>
+      </li>
+      <li class="hidden-lg hidden-md">
+        <router-link to="/product" @click.native="closeNavBarFromChild">Product</router-link>
+      </li>
+    </ul>
+  </nav>
+</nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
+
